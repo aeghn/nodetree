7c0f11761a905a8452a37e158daab14ced83d08c
aeghn <aeghn@outlook.com>
2024-03-19 18:03:33 +0800

    feat(ntcore): add MagicConstants

--- nodetree-core/src/model/mod.rs
+++ nodetree-core/src/model/mod.rs
@@ -1,5 +1,22 @@
+use serde::{Deserialize, Serialize};
+
+use crate::constants;
+
 pub mod alarm;
 pub mod node;
 pub mod nodefilter;
 pub mod tag;
 pub mod todo;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct MagicConstants {
+    pub root: String,
+}
+
+impl Default for MagicConstants {
+    fn default() -> Self {
+        Self {
+            root: constants::MAGIC_ROOT_NODE_ID.to_string(),
+        }
+    }
+}
